36cab529bb83dfe7da843c4a0b0ecba15f9ead45
вонгуендангхоа <129539631+chimotcau@users.noreply.github.com>
2023-11-12 00:40:56 +0300

    hw2

--- exercise2.cpp
+++ exercise2.cpp
@@ -1,32 +1,16 @@
 #include<iostream>
 bool overlap(int x1,int y1,int x2,int y2, int X1, int Y1, int X2, int Y2);
 int main(){
-    int x1;
-    int y1;
-    int x2;
-    int y2;
-    int X1;
-    int Y1;
-    int X2;
-    int Y2;
- 
+    int x1,y1,x2,y2,X1,Y1,X2,Y2;
     std::cout<<"enter the coordinate of x1, y1, x2, y2, X1, Y1, X2, Y2 corresponding"<<'\n';
-        
-
-    
     std::cin>>x1>>y1>>x2>>y2>>X1>>Y1>>X2>>Y2;
-
     overlap(x1,y1,x2,y2,X1,Y1,X2,Y2);
-    
-    
-
     return 0;
 }
 bool overlap(int x1,int y1,int x2,int y2, int X1, int Y1, int X2, int Y2){
     if((Y1>=y2) || (X1>=x2) || (y1>=Y2) || (x1>=X2) ){
         std::cout<<"2 shapes are not overlaping";
         
-
     }
     else{
         std::cout<<"2 shapes are overlaping";
---
+++ exercise3.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<cmath>
+void swap(double& a, double& b) {
+    double c = a;
+    a = b;
+    b = c;
+}
+void swapindex(int& a, int& b) {
+    int c = a;
+    a = b;
+    b = c;
+}
+
+void bubbleSort(double array[], int indices[], int n) {
+    for (int i = 0; i < n - 1; ++i) {
+        for (int j = 0; j < n - i - 1; ++j) {
+            if (array[j] > array[j + 1]) {
+                swap(array[j], array[j + 1]);
+                swapindex(indices[j], indices[j + 1]);
+            }
+        }
+    }
+}
+
+int main(){
+    int n;
+    int k;
+
+    std::cout<<"enter number of lines ";
+    std::cin>>n;
+    std::cout<<"enter k ";
+    std::cin>>k;
+    int** coor = new int*[n];
+    for (int i = 0; i < n; ++i) {
+        coor[i] = new int[2];
+    }
+    std::cout << "Enter x and y:\n";
+    for (int i = 0; i < n; ++i) {
+        std::cin >> coor[i][0] >> coor[i][1];
+    }
+    double *a= new double[n];
+    int *index= new int[n];
+    for(int i=0;i<n;i++){
+        a[i]= sqrt(coor[i][0]*coor[i][0]+coor[i][1]*coor[i][1]); 
+        index[i] = i;
+        std::cout<<a[i]<<'\n';      
+    }
+    bubbleSort(a, index, n);
+    for (int i = 0; i < k; ++i) {
+        int originaloder = index[i];
+        std::cout << coor[originaloder][0] << ' ' << coor[originaloder][1] << '\n';
+    }
+    delete[] a;
+    delete[] index;
+    for (int i = 0; i < n; ++i) {
+        delete[] coor[i];
+    }
+    delete[] coor;
+    return 0;
+}
